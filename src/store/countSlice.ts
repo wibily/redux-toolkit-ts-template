d6bb5123d8562027c76109d688414cc672597eec
William Cheung <william.cheung@data61.csiro.au>
2020-03-25 15:28:33 +1000

    fixed type error in reducers

--- src/store/countSlice.ts
+++ src/store/countSlice.ts
@@ -4,8 +4,8 @@ const count = createSlice({
   name: "count",
   initialState: 0,
   reducers: {
-    inc: (state, action: PayloadAction<number>) => state + 1,
-    dec: (state, action: PayloadAction<number>) => state - 1
+    inc: (state, action: PayloadAction<void>) => state + 1,
+    dec: (state, action: PayloadAction<void>) => state - 1
   }
 });
 
